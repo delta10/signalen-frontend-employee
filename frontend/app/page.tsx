0b6d89f6e15f5d2ff82820e229e1cd1b34b828dd
HugoSchiereck <h.schiereck@draads.nl>
2025-10-06 09:34:11 +0200

    merge  'components' into 'First-table'
    Imported and prepared table components in Home page for future use. Updated Tailwind CSS and added autoprefixer and postcss to devDependencies for improved CSS processing.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,16 @@
 import Image from "next/image";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 import { Button } from "@/components/ui/button"
- 
+
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
@@ -55,6 +65,13 @@ export default function Home() {
           </a>
         </div>
       </main>
+
+
+
+
+
+
+
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
